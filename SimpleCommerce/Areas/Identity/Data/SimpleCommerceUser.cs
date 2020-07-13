669c2816172398d014eda2b29952a9b8758c62bf
Mattias Hammarsten <mattias@hammarsten.nu>
2020-07-13 20:03:49 +0200

    Initial commit

---
+++ SimpleCommerce/Areas/Identity/Data/SimpleCommerceUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace SimpleCommerce.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the SimpleCommerceUser class
+    public class SimpleCommerceUser : IdentityUser
+    {
+    }
+}
---
+++ SimpleCommerce/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SimpleCommerce.Areas.Identity.Data;
+using SimpleCommerce.Data;
+
+[assembly: HostingStartup(typeof(SimpleCommerce.Areas.Identity.IdentityHostingStartup))]
+namespace SimpleCommerce.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<IdentityContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("IdentityContextConnection")));
+
+                services.AddDefaultIdentity<SimpleCommerceUser>()
+                    .AddEntityFrameworkStores<IdentityContext>();
+            });
+        }
+    }
+}
